484df7ac9bc85291ee398aa9697734fd98085690
Nishanth6153 <nishant40y6153@gmail.com>
2026-06-03 15:09:05 +0530

    03-06-2026

---
+++ day 3/JobScheduling.java
@@ -0,0 +1,25 @@
+public class JobScheduling {
+
+    public static void main(String[] args) {
+
+        String[] jobs = {"A", "B", "C"};
+        int[] deadline = {2, 1, 3};
+        int[] profit = {200, 100, 500};
+
+        boolean[] slot = new boolean[4];
+        int totalProfit = 0;
+
+        for (int i = 0; i < jobs.length; i++) {
+
+            if (!slot[deadline[i]]) {
+                slot[deadline[i]] = true;
+                totalProfit += profit[i];
+
+                System.out.println("Scheduled Job: " + jobs[i] + " with profit: " + profit[i]);
+                
+            }
+        }
+
+        System.out.println("Maximum Profit = " + totalProfit);
+    }
+}
